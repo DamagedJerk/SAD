5ef8df324d831d121e4ece16bbce46b88e805e1e
Brent Jake <brentjake_teleron@umindanao.edu.ph>
2019-07-28 20:26:53 +0800

    CLOSINGBUTTON

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -1,6 +1,8 @@
 package Project;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,15 +11,25 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    private Stage stage;
+    private JFXButton btnClose= new JFXButton();
     @Override
     public void start(Stage primaryStage) throws Exception{
+        stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("Log-InForm.fxml"));
 
-        primaryStage.setScene(new Scene(root));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.show();
-    }
+        stage.setScene(new Scene(root));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
 
+
+    }
+    @FXML
+    private void close(){
+        stage = (Stage) btnClose.getScene().getWindow();
+        stage.close();
+        System.exit(1);
+    }
 
     public static void main(String[] args) {
         launch(args);
